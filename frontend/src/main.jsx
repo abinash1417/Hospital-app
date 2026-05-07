8cd87fcb8b3466a3fb967df2c1038d932a142937
abinash1417 <chandrannash281418@gmail.com>
2026-05-07 09:39:51 +0530

    Add React app entry point with providers

---
+++ frontend/src/main.jsx
@@ -0,0 +1,18 @@
+import React from 'react'
+import ReactDOM from 'react-dom/client'
+import App from './App.jsx'
+import './index.css'
+import { AuthProvider } from './context/AuthContext'
+import { SocketProvider } from './context/SocketContext'
+import { Toaster } from 'react-hot-toast'
+
+ReactDOM.createRoot(document.getElementById('root')).render(
+  <React.StrictMode>
+    <AuthProvider>
+      <SocketProvider>
+        <Toaster position="top-right" />
+        <App />
+      </SocketProvider>
+    </AuthProvider>
+  </React.StrictMode>
+)
